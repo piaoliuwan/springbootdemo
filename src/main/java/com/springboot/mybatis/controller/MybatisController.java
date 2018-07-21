2abbaf55198c2b80fb16afd719a19d47de12e7d9
piaoliuwan <1304427517@qq.com>
2018-07-21 17:22:49 +0800

    spring boot整合redis             成功版

--- src/main/java/com/springboot/mybatis/controller/MybatisController.java
+++ src/main/java/com/springboot/mybatis/controller/MybatisController.java
@@ -1,9 +1,11 @@
 package com.springboot.mybatis.controller;
 
+import com.alibaba.druid.support.json.JSONUtils;
 import com.springboot.mybatis.mapper.GirlMapper;
 import com.springboot.mybatis.pojo.Girl;
 import com.springboot.mybatis.service.GirlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,8 @@ import java.util.List;
 
 @RestController
 public class MybatisController {
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @Autowired
     private GirlService  girlService;
 
@@ -32,5 +35,16 @@ public class MybatisController {
     @GetMapping(value = "mybatis/myself")
     public Girl findgirlmyself(){
         return girlService.myselfSelect("1");
+    }
+
+    @GetMapping(value = "redis/test")
+    public String testRedis(){
+        Girl girl = new Girl();
+        girl.setMoney(432.8);
+        girl.setCupSize("D");
+        girl.setAge(24);
+        //String json = JSONUtils.toJSONString(girl);
+        stringRedisTemplate.opsForValue().set("girl","1235");
+        return stringRedisTemplate.opsForValue().get("girl");
     }
 }
